03c333a89097ff56c603f4b871be2edfca782e5b
LavaSnake <LavaSnake@users.noreply.github.com>
2014-02-24 10:50:29 -0500

    Updated profile pic fixer: Support for displaying the player's PA Stats Ladder info and a bug fix

--- LavaSnakes_ProfilePicFixer/ui/mods/LavaSnakes_ProfilePicFixer/ProfilePicFixer.js
+++ LavaSnakes_ProfilePicFixer/ui/mods/LavaSnakes_ProfilePicFixer/ProfilePicFixer.js
@@ -12,28 +12,42 @@ $(function () {
 		
 		var idLoc = xhr.responseText.indexOf("<link rel=\"canonical\" href=\"https://forums.uberent.com/members/") + 66;
 		idLoc = xhr.responseText.indexOf(".", idLoc) + 1;
-		var id = xhr.responseText.substring(idLoc, xhr.responseText.indexOf("\"", idLoc) - 1);
-		console.log("Pic found to use as default: https://forums.uberent.com/data/avatars/l/1943/" + id + ".jpg");
+		var id = xhr.responseText.substring(idLoc, xhr.responseText.indexOf("/", idLoc));
+		console.log("Pic found to use as default: https://forums.uberent.com/data/avatars/l/" + id.substr(0, 4) + "/" + id + ".jpg");
 
-		window.localStorage.LProfilePic_URL = "https://forums.uberent.com/data/avatars/l/1943/" + id + ".jpg";
+		window.localStorage.LProfilePic_URL = "https://forums.uberent.com/data/avatars/l/" + id.substr(0, 4) + "/" + id + ".jpg";
+	}
+	
+	if (!window.localStorage.LProfilePic_ShowPAS) {
+		window.localStorage.LProfilePic_ShowPAS = "true";
 	}
 
+	//Mod config menu
 	$('body').append('<span id="PicConfigSpan"></span>');
 	elements[0].style.background = "url(" + window.localStorage.LProfilePic_URL + ") center center #333";
 	elements[0].style.backgroundSize = "80px 80px";
 	elements[0].onclick = function() { 
 		//Show dialog
+		var CheckedState = "";
+		if (window.localStorage.LProfilePic_ShowPAS == "true") {
+			CheckedState = " checked ";
+		}
+		
 		document.getElementById('PicConfigSpan').innerHTML = 
-			'<div style="margin: 0px auto; position: absolute; background: rgba(0,0,0,.5); padding: 10px; left: 20px; top: 200px; right: 20px; height: 100px; width: 500px; border: 1px solid rgba(255,255,255,.1);	border-radius: 4px;">' +
+			'<div style="margin: 0px auto; position: absolute; background: rgba(0,0,0,.5); padding: 10px; left: 20px; top: 200px; right: 20px; height: 130px; width: 500px; border: 1px solid rgba(255,255,255,.1);	border-radius: 4px;">' +
 				'<div style="margin: 0px 0px 4px 0px; font-weight: bold; font-size: 1.2em;"> SET PROFILE PICTURE ' + 
 					'<a data-bind="click_sound: \'default\'" onclick="LSPPF_updateAndClose();"><img style="float:right;" src="../shared/img/close_btn.png"></a>' +
 				'</div>' +
 				'<p>URL: ' +
 					'<input style="width: 475px;" type="text" value="' + window.localStorage.LProfilePic_URL + '" onchange="window.localStorage.LProfilePic_URL = this.value">' +
 				'</p>' +
+				'<p>' +
+					'<input type="checkbox"' + CheckedState + 'onchange="window.localStorage.LProfilePic_ShowPAS = this.checked">Show PA Stats Ladder Data' +
+				'</p>' +
 			'</div>';
 	};
 
+	//Do some Awesome?
 	var DoAwesome = Math.floor((Math.random()*5)+1);
 
 	if (DoAwesome == 5) {
@@ -70,11 +84,30 @@ $(function () {
 			+ Awesome + 
 			'</span></a></span></td></tr>');
 	}
+	
+	//Add ladder pic
+	if (window.localStorage.LProfilePic_ShowPAS == "true") {
+		$('#A12').parent().parent().parent().before(
+				'<tr id="LProfilePic_PAS"><td class="td_start_menu_item"><span class="link_start_menu_item"><a href="#" id="A8" data-bind="click_sound: \'default\', rollover_sound: \'default\'"><span class="start_menu_item_lbl"><img src="http://pastats-ladder.gamestown24.de/avatars/' + model.displayName() + '.png"></span></a></span></td></tr>');
+	}
 });
 
 function LSPPF_updateAndClose() {
+	//Update pic to new setting
 	var elements = document.getElementsByClassName("div_start_menu_profile_pic");
-	document.getElementById('PicConfigSpan').innerHTML = '';
 	elements[0].style.background = "url(" + window.localStorage.LProfilePic_URL + ") center center #333";
 	elements[0].style.backgroundSize = "80px 80px";
+	
+	//Update ladder pic to new setting
+	if ($("#LProfilePic_PAS").length > 0 && window.localStorage.LProfilePic_ShowPAS == "false") {
+		//It is in the UI and needs to be removed
+		$("#LProfilePic_PAS").remove();
+	} else if (!$("#LProfilePic_PAS").length > 0 && window.localStorage.LProfilePic_ShowPAS == "true") {
+		//It is not in the UI and needs to be added
+		$('#A12').parent().parent().parent().before(
+				'<tr id="LProfilePic_PAS"><td class="td_start_menu_item"><span class="link_start_menu_item"><a href="#" id="A8" data-bind="click_sound: \'default\', rollover_sound: \'default\'"><span class="start_menu_item_lbl"><img src="http://pastats-ladder.gamestown24.de/avatars/' + model.displayName() + '.png"></span></a></span></td></tr>');
+	}
+	
+	//Close
+	document.getElementById('PicConfigSpan').innerHTML = '';
 }
