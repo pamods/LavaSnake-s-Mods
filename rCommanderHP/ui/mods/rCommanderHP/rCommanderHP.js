655a12814e744c820c59d2d5f3a27ada4759de6e
LavaSnake <LavaSnake@users.noreply.github.com>
2014-05-03 22:06:28 -0400

    Cleaned up code and rewrote CSS for Commander HUD

--- rCommanderHP/ui/mods/rCommanderHP/rCommanderHP.js
+++ rCommanderHP/ui/mods/rCommanderHP/rCommanderHP.js
@@ -1,21 +1,19 @@
 (function() {
 	initialSettingValue('commander_hp_display_show', 'ALWAYS');
 
-        bif.registerBIFReadyCallback(function () {
-        
-        
-        });
-
-	//var commanderImages = new Array('img/build_bar/units/imperial_delta.png', 'img/build_bar/units/imperial_alpha.png', 'img/build_bar/units/raptor_base.png', 'img/build_bar/units/quad_base.png', 'img/build_bar/units/tank_base.png');
+	bif.registerBIFReadyCallback(function () {
+		//var commanderImages = new Array('img/build_bar/units/imperial_delta.png', 'img/build_bar/units/imperial_alpha.png', 'img/build_bar/units/raptor_base.png', 'img/build_bar/units/quad_base.png', 'img/build_bar/units/tank_base.png');
+		
+	});
+	
 	var settings = decode(localStorage.settings);
-
 	model.commander_hp_display_show = ko.computed(function() { return settings['commander_hp_display_show'] == 'ALWAYS'});
 
 	createFloatingFrame('commander_info_frame', 265, 50, {'offset': 'topRight', 'left': -265});
 
 	$('#commander_info_frame_content').append(
 		'<div class="div_commander_bar" data-bind="visible: model.armySize() > 0 && (  model.armyAlertModel.commanderUnderAttackAlert.test(model.commanderHealth()) !== undefined || model.commander_hp_display_show())">' + //Only display commander info bar if player isn't dead (has more than 1 unit).
-			'<div class ="div_commander_bar_cont">' +
+			'<div class="div_commander_bar_cont">' +
 			'<table>' +
 				'<tbody>' +
 					'<tr data-bind="click: function() { api.select.commander(); api.camera.track(true); }">' +
